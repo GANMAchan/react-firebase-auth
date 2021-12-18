700f230efbaeece889534aab0101751460716e57
unknown <kagach113@yahoo.co.jp>
2021-12-19 01:42:10 +0900

    router設定

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
+import Home from './components/Home'
 import SignUp from './components/SignUp'
+import Login from './components/Login'
 import { AuthProvider } from './context/AuthContext'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
     return (
         <AuthProvider>
-            <div style={{ margin: '2em' }}>
-                <SignUp />
-            </div>
+            <div style={{ margin: '2em' }}></div>
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<Home />} path='/' component={Home} />
+                    <Route
+                        element={<SignUp />}
+                        path='/signup'
+                        component={SignUp}
+                    />
+                    <Route
+                        element={<Login />}
+                        path='/login'
+                        component={Login}
+                    />{' '}
+                </Routes>
+            </BrowserRouter>
         </AuthProvider>
     )
 }
