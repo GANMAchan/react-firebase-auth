9db4f22209c590fbc624dca24a8ef1d6cbde08f0
unknown <kagach113@yahoo.co.jp>
2021-12-19 02:28:39 +0900

    ホームとログイン作成

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,52 @@
+import { useState } from 'react'
+import { auth } from '../firebase'
+import { Link } from 'react-router-dom'
+
 const Login = () => {
-    return <h1>ログイン画面</h1>
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        auth.createUserWithEmailAndPassword(email, password)
+    }
+    const handleChangeEmail = (event) => {
+        setEmail(event.currentTarget.value)
+    }
+    const handleChangePassword = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+
+    return (
+        <div>
+            <h1>ログイン</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>メールアドレス</label>
+                    <input
+                        name='email'
+                        type='email'
+                        placeholder='email'
+                        onChange={(event) => handleChangeEmail(event)}
+                    />
+                </div>
+                <div>
+                    <label>パスワード</label>
+                    <input
+                        name='password'
+                        type='password'
+                        placeholder='password'
+                        onChange={(event) => handleChangePassword(event)}
+                    />
+                </div>
+                <div>
+                    <button>ログイン</button>
+                </div>
+                <div>
+                    <Link to={'/signup'}>ユーザー登録</Link>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default Login
