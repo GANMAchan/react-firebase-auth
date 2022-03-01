d58a63db862d04d8b9b46c977a5943d77dc196a7
unknown <kagach113@yahoo.co.jp>
2022-03-02 01:55:50 +0900

    PrivaveRoute諦め

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import { auth } from '../firebase'
 import { useNavigate, Navigate } from 'react-router-dom'
+import { useAuthContext } from '../context/AuthContext'
 
 const Home = () => {
     const navigate = useNavigate()
@@ -7,13 +8,19 @@ const Home = () => {
         auth.signOut()
         navigate('/login')
     }
+    const { user } = useAuthContext()
+    console.log(user)
 
-    return (
-        <div>
-            <h1>ホームページ</h1>
-            <button onClick={handleLogout}>ログアウト</button>
-        </div>
-    )
+    if (!user) {
+        return <Navigate to='/login' />
+    } else {
+        return (
+            <div>
+                <h1>ホームページ</h1>
+                <button onClick={handleLogout}>ログアウト</button>
+            </div>
+        )
+    }
 }
 
 export default Home
